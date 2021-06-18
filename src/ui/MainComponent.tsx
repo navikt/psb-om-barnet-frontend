38e507ae4dc26eb8b4cf1b5bf7c2c0f7acf7765b
Hallvard Andreas Stark <hallvard@scel.to>
2021-06-18 14:06:04 +0200

    separert ut controller rydding

--- src/ui/MainComponent.tsx
+++ src/ui/MainComponent.tsx
@@ -1,41 +1,38 @@
 import React from 'react';
-import Alertstripe from 'nav-frontend-alertstriper';
-import OmBarnetForm from './OmBarnetForm';
-import './styles.css';
+import ContainerContract from '../types/ContainerContract';
 import ContainerContext from './context/ContainerContext';
+import OmBarnetController from './OmBarnetController';
+import './styles.css';
 
-const MainComponent = ({ data }) => {
+interface MainComponentProps {
+    data: ContainerContract;
+}
+
+const MainComponent = ({ data }: MainComponentProps): JSX.Element => {
     return (
         <ContainerContext.Provider value={data}>
             <h1 className="text-3xl font-semibold">Om barnet</h1>
             <div className="flex mt-10">
-                <div className="mr-7">
-                    <p>
-                        Navn:
-                        <span className="font-semibold ml-1">Snill Blyant</span>
-                    </p>
-                </div>
-                <div className="mr-7">
-                    <p>
-                        Fødselsnummer:
-                        <span className="font-semibold ml-1">00000000000</span>
-                    </p>
-                </div>
-                <div className="mr-7">
-                    <p>
-                        Diagnose:
-                        <span className="font-semibold ml-1">Diagnose</span>
-                    </p>
-                </div>
-                <div className="mr-7 text-white bg-black rounded pr-2 pl-2">Død 01.01.2021</div>
+                <p className="mr-7">
+                    Navn:
+                    <span className="font-semibold ml-1">Snill Blyant</span>
+                </p>
+
+                <p className="mr-7">
+                    Fødselsnummer:
+                    <span className="font-semibold ml-1">00000000000</span>
+                </p>
+
+                <p className="mr-7">
+                    Diagnose:
+                    <span className="font-semibold ml-1">Diagnose</span>
+                </p>
+                <p className="mr-7 text-white bg-black rounded pr-2 pl-2">Død 01.01.2021</p>
             </div>
             <div className="mt-6 pt-6 border-t border-gray-300">
                 <h2 className="text-xl font-semibold">Rett til pleiepenger ved barnets død</h2>
-                <Alertstripe type="advarsel" className="mt-6">
-                    Vurder hvor lang periode søker har rett på pleiepenger ved barnets død.
-                </Alertstripe>
+                <OmBarnetController />
             </div>
-            <OmBarnetForm />
         </ContainerContext.Provider>
     );
 };
