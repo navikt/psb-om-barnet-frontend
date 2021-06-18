10fb3cd66143a0ba5b2a24e60b652bb2c83de589
Hallvard Andreas Stark <hallvard@scel.to>
2021-06-18 12:00:35 +0200

    la til label for å vise dødsdato

--- src/ui/MainComponent.tsx
+++ src/ui/MainComponent.tsx
@@ -5,28 +5,29 @@ import './styles.css';
 
 const MainComponent = ({ data }) => (
     <div>
-        <h1 className="text-3xl font-semibold mb-10">Om barnet</h1>
-        <div className="flex pb-6 border-b border-gray-300">
-            <div className="flex mr-7">
+        <h1 className="text-3xl font-semibold">Om barnet</h1>
+        <div className="flex mt-10">
+            <div className="mr-7">
                 <p>
                     Navn:
                     <span className="font-semibold ml-1">Snill Blyant</span>
                 </p>
             </div>
-            <div className="flex mr-7">
+            <div className="mr-7">
                 <p>
                     Fødselsnummer:
                     <span className="font-semibold ml-1">00000000000</span>
                 </p>
             </div>
-            <div className="flex mr-7">
+            <div className="mr-7">
                 <p>
                     Diagnose:
                     <span className="font-semibold ml-1">Diagnose</span>
                 </p>
             </div>
+            <div className="mr-7 text-white bg-black rounded pr-2 pl-2">Død 01.01.2021</div>
         </div>
-        <div className="mt-6">
+        <div className="mt-6 pt-6 border-t border-gray-300">
             <h2 className="text-xl font-semibold">Rett til pleiepenger ved barnets død</h2>
             <Alertstripe type="advarsel" className="mt-6">
                 Vurder hvor lang periode søker har rett på pleiepenger ved barnets død.
